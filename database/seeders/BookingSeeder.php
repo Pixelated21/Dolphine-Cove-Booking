3b47c16e8bd2a1a31a0f1dcc47ce425368bd2c10
j4v3l <37094748+j4v3l@users.noreply.github.com>
2021-09-16 12:54:20 -0500

    Seeders
    Add data

--- database/seeders/BookingSeeder.php
+++ database/seeders/BookingSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Booking;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class BookingSeeder extends Seeder
 {
@@ -11,8 +13,35 @@ class BookingSeeder extends Seeder
      *
      * @return void
      */
+    protected $booking = [
+        [
+            'booking_id' => 1,
+            'guest_id' => 1,
+            'tour_company_id' => 1,
+            'programme_id' => 1,
+            'booking_type' => "walk-in",
+            'payment_type' => "cash",
+            'date_booked' => "2021-09-16 08:18:13",
+            'created_at' => "2021-09-16 08:18:13",
+            'updated_at' => "2021-09-16 08:18:13"
+        ],
+        [
+            'booking_id' => 2,
+            'guest_id' => 2,
+            'tour_company_id' => 2,
+            'programme_id' => 2,
+            'booking_type' => "online",
+            'payment_type' => "credit",
+            'date_booked' => "2021-09-16 08:18:13",
+            'created_at' => "2021-09-16 08:18:13",
+            'updated_at' => "2021-09-16 08:18:13"
+        ]
+    ];
+
     public function run()
     {
-        //
+        foreach ($this->booking as $bookings) {
+            Booking::create($bookings);
+        }
     }
 }
